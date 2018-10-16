702b0fa9a24360b384c93ce1b21553422406c865
sgoyet972 <scott_goyette@comcast.com>
2018-10-15 21:52:50 -0400

    Markup for our standups and navigation

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -10,8 +10,23 @@ class ActivityController < ApplicationController
       .order('standup_date DESC')
       .page(params[:page])
       .per(6)
+
+
+    @teams =
+      current_user
+      .teams
+      .includes(:days)
+      .where(has_reminder: true)
+      .where('days_of_the_week_memberships.day = ?', current_day)
+      .references(:days_of_the_week_memberships)
   end
 
   def feed
   end
+
+  private
+
+    def current_day
+      DaysOfTheWeekMembership.days[Time.now.utc.strftime('%A').downcase.to_sym]
+    end
 end
